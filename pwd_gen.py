a45d8a10e8d9e5122bc041500b552e7a96994582
Stephen Podlogar <stpodlogar@gmail.com>
2021-12-09 22:06:04 -0500

    first commit

---
+++ pwd_gen.py
@@ -0,0 +1,28 @@
+import secrets
+import string
+
+def generate_pwd():
+    print('\nWelcome to PSWRD Generator')
+    print('=' * 26 + '\n')
+
+    #get user input for length of password to generate
+    length = int(input('Please enter the desired password length: '))
+
+    #define data
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    num = string.digits
+    symbols = '!@#$%^&*'
+
+    #combine data
+    chars = lower + upper + num + symbols
+
+    #generate array of random chars
+    temp = (secrets.choice(chars) for char in range(length))
+
+    #combine temp array to create password
+    password = ''.join(temp)
+
+    print(password)
+
+generate_pwd()
