cbc8bbfc35a278fb14af28f8ab286a060aa6fb67
Stephen Podlogar <stpodlogar@gmail.com>
2021-12-14 10:21:14 -0500

    implement minimum number of digits and special characters

--- pwd_gen.py
+++ pwd_gen.py
@@ -1,5 +1,7 @@
 import secrets
 import string
+import random
+import array
 
 def generate_pwd():
     print('\nWelcome to PSWRD Generator')
@@ -13,15 +15,35 @@ def generate_pwd():
     upper = string.ascii_uppercase
     num = string.digits
     symbols = '!@#$%^&*'
-
-    #combine data
+    #combine all characters
     chars = lower + upper + num + symbols
 
-    #generate array of random chars
-    temp = (secrets.choice(chars) for char in range(length))
+    #get minimum number of digits and special chars from user
+    num_digits = int(input('Minimum numbers: '))
+    num_special = int(input('Minimum special characters: '))
+    #initialize the required number or specific characters
+    rand_digit = str()
+    rand_special = str()
+    rand_upper = (secrets.choice(upper))
+    rand_lower = (secrets.choice(lower))
+
+    for _ in range(num_digits):
+        rand_digit += secrets.choice(num)
+
+    for _ in range(num_special):
+        rand_special += secrets.choice(symbols)
+
+    temp_pass = rand_digit + rand_special + rand_upper + rand_lower
+
+    for _ in range(length - (num_digits + num_special + 2)):
+        temp_pass += secrets.choice(chars)
+
+    #convert tmp_pass to list and shuffle to prevent from a consistent pattern forming
+    temp_pass_list = list(temp_pass)
+    random.shuffle(temp_pass_list)
 
     #combine temp array to create password
-    password = ''.join(temp)
+    password = ''.join(temp_pass_list)
 
     print(f'\n{password}\n')
 
